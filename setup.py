0b7e023c06c37da32505b0a3239a63aad7917070
phoneintel <phoneintel@proton.me>
2024-08-07 18:11:40 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='phoneintel',
-    version='1.0',
+    version='1.0.8',
     description='A tool for processing phone numbers with OSINT capabilities.',
     author='PhoneIntel',
     author_email='phoneintel@proton.me',
