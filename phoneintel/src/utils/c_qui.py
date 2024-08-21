2f3c5829685ca830ce6d2df0d448ea5479342f5b
phoneintel <phoneintel@proton.me>
2024-08-21 13:59:14 -0300

    Update c_qui.py

--- phoneintel/src/utils/c_qui.py
+++ phoneintel/src/utils/c_qui.py
@@ -4,14 +4,30 @@
 #      CREATED BY THE PHONEINTEL TEAM    #
 #                                        #
 ##########################################
-#                                        #
 # ALL INFORMATION IS SOURCED EXCLUSIVELY #
 #      FROM OPEN SOURCE AND PUBLIC       #
 #               RESOURCES                #
 #                                        #
+#   THIS SOFTWARE IS PROVIDED "AS IS",   #
+#   WITHOUT WARRANTY OF ANY KIND,        #
+#   EXPRESS OR IMPLIED, INCLUDING BUT    #
+#   NOT LIMITED TO THE WARRANTIES OF     #
+#   MERCHANTABILITY, FITNESS FOR A       #
+#   PARTICULAR PURPOSE AND               #
+#   NONINFRINGEMENT.                     #
+#                                        #
+#   IN NO EVENT SHALL THE AUTHORS OR     #
+#   COPYRIGHT HOLDERS BE LIABLE FOR ANY  #
+#   CLAIM, DAMAGES OR OTHER LIABILITY,   #
+#   WHETHER IN AN ACTION OF CONTRACT,    #
+#   TORT OR OTHERWISE, ARISING FROM,     #
+#   OUT OF OR IN CONNECTION WITH THE     #
+#   SOFTWARE OR THE USE OR OTHER         #
+#   DEALINGS IN THE SOFTWARE.            #
+#                                        #
 #     THIS NOTICE MUST REMAIN INTACT     #
 #   FOR CODE REDISTRIBUTION UNDER THE    #
-#           APACHE 2.0 LICENSE           #
+#           GPL-3.0 license              #
 #                                        #
 ##########################################
 
