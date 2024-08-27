cafdbe336745509983191b30f242d7f15420add5
phoneintel <phoneintel@proton.me>
2024-08-27 11:58:39 -0300

    Update api_utils.py

--- phoneintel/src/utils/api_utils.py
+++ phoneintel/src/utils/api_utils.py
@@ -38,7 +38,8 @@ from colorama import Fore
 api_list_params = {
 
     "neutrino_id" : "",
-    "neutrino_key": ""
+    "neutrino_key": "",
+    "caller_id_key": ""
 
 
 }
@@ -47,7 +48,23 @@ def check_api_list()->bool:
     try:
         if path.exists(USER_API_KEYS):
 
-            return True
+            with open(USER_API_KEYS, 'r', encoding='utf-8') as file:
+                file = load(file)
+            
+            if file == api_list_params:
+                
+                return True
+            else:
+                try:
+                    with open(USER_API_KEYS, 'w', encoding='utf-8') as file:
+                        dump(api_list_params, file, indent=4)
+                        
+                    return True
+                
+                except:
+                    print(f"{Fore.RED}[ERROR] Can't create api_list.json in {USER_API_KEYS}")
+                    return False
+                
         
         else:
 
